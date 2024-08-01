5bfce047010a88d3b53f7f1c495b07008e7b33f4
patand08 <patrickandrade08@hotmail.com>
2024-08-01 18:36:03 -0300

    get cropped images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,8 +5,7 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color =
-    score > 75 ? "green" : score > 60 ? "yellow" : score < 50 ? "red" : "";
+  let color = score > 75 ? "green" : score > 50 ? "yellow" : "red";
 
   return (
     <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
