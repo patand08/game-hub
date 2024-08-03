6e16a1c0adb443a17165c129e4c9547663e2a3fb
patand08 <patrickandrade08@hotmail.com>
2024-08-03 14:42:08 -0300

    genres/platform as static props for shipping

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <GameCardContainer>
-      <Card>
+      <Card height="100%">
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <HStack justifyContent={"space-between"}>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }}
         spacing={6}
         padding="10px"
       >
