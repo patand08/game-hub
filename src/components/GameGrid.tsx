3189b517c354f1e8b6f28657ce4314cd53e63db3
patand08 <patrickandrade08@hotmail.com>
2024-08-03 14:58:30 -0300

    Grid no fragment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,18 +12,17 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, loading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {loading && skeletons.map((skel) => <GameCardSkeleton key={skel} />)}
-        {!loading && data.map((game) => <GameCard key={game.id} game={game} />)}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {loading && skeletons.map((skel) => <GameCardSkeleton key={skel} />)}
+      {!loading && data.map((game) => <GameCard key={game.id} game={game} />)}
+    </SimpleGrid>
   );
 };
 
