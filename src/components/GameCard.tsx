032cadf15591e445b090bad008f29121b05d8986
patand08 <patrickandrade08@hotmail.com>
2024-08-12 15:15:06 -0300

    Change cards behavior

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -22,27 +22,28 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <GameCardContainer>
-      <Card height="100%">
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-          <HStack justifyContent={"space-between"}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          <Heading fontSize="2xl">
-            <Box
-              _hover={{
-                textColor: "gray",
-              }}
-            >
-              <Link to={"/games/" + game.slug}>{game.name}</Link>
-            </Box>
-            <Emoji rating={game.rating_top} />
-          </Heading>
-        </CardBody>
-      </Card>
+      <Link to={"/games/" + game.slug}>
+        <Card
+          height="100%"
+          _hover={{
+            textColor: "gray",
+          }}
+        >
+          <Image src={getCroppedImageUrl(game.background_image)} />
+          <CardBody>
+            <HStack justifyContent={"space-between"}>
+              <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+              />
+              <CriticScore score={game.metacritic} />
+            </HStack>
+            <Heading fontSize="2xl">
+              <Box>{game.name}</Box>
+              {/* <Emoji rating={game.rating_top} /> I dont want the emojis*/}
+            </Heading>
+          </CardBody>
+        </Card>
+      </Link>
     </GameCardContainer>
   );
 };
